0c8a58d95e4ed6b293b39cdafe7dad3e9b4f07a8
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-08-12 00:18:10 +0300

    Реализована обработка обратной связи

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,9 @@ def home(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({phone}): {message}')
     return render(request, 'contact.html')
