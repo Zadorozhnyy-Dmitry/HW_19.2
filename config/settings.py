2e93f22b81650c1f2311bca654d86d302f39d588
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-08-20 23:16:52 +0300

    HW_20.1 task_1 Установил psycopg2, создал базу данных и изменил настройки для подключения к ней

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hw_20.1',
+        'USER': 'postgres',
+        'PASSWORD': 'lvbnhbq',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
