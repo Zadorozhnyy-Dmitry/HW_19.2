0dd82c222e7b26cf9524c2ebfa9c0bab61a8615a
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-09-05 22:34:18 +0300

    HW_21.1 (3) Вывод статей, которые имеют публикации

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,11 @@ class BlogUpdateView(UpdateView):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
