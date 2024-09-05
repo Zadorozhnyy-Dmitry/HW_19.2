90a2cab5e49211721368059d2240c2f194ad2d3e
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-09-05 22:27:56 +0300

    HW_21.1 (3) реализовал счетчик просмотров

--- blog/views.py
+++ blog/views.py
@@ -24,6 +24,12 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Blog
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class BlogDeleteView(DeleteView):
     model = Blog
