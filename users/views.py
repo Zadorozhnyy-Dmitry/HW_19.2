bcf34df18ba5448751f45894417c6e7056fb0827
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-09-20 22:15:45 +0300

    HW_22.2 (2) Регистрация пользователя по почте

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from users.forms import UserRegisterForm
+from users.models import User
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    """
+    Контроллер для регистрации пользователя
+    """
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
