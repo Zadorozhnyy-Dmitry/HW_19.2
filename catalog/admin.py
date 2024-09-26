037900739771c0d2e43457b851373207da7a2436
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-09-26 23:25:08 +0300

    HW_23.1 если у тавара статус опубликован не стоит, то в отображение не попадает

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,7 +5,7 @@ from catalog.models import Product, Category, Version
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "category", "owner")
+    list_display = ("id", "name", "price", "category", "owner", "is_published")
     list_filter = ("category",)
     search_fields = ("name", "description")
 
